9376b7d7be566ab173d99f62ac33614a56ad3cd8
Nataphan rakvidhyasatra <85008989+NRdeARK@users.noreply.github.com>
2022-10-26 05:08:49 +0700

    Update qdata.js

--- qdata.js
+++ qdata.js
@@ -4,8 +4,8 @@ var url = "mongodb://localhost:27017/";
 MongoClient.connect(url, function(err, db) {
   if (err) throw err;
   var dbo = db.db("mydb");
-  var query = { address: "Park Lane 38" };
-  dbo.collection("customers").find(query).toArray(function(err, result) {
+  var query = { address: "0223" };
+  dbo.collection("admin").find(query).toArray(function(err, result) {
     if (err) throw err;
     console.log(result);
     db.close();
